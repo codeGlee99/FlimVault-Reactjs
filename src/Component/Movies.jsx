d757666c5685a4f12b2490afff70a7d68c4b31b3
sangeetha <gopisakthivel@alphabsolutions.com>
2024-02-28 21:35:04 +0530

    added data using api

--- src/Component/Movies.jsx
+++ src/Component/Movies.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MoviesCard from './MoviesCard'
+import axios from "axios";
 
 function Movies() {
+
+  const[Movies,setMovies] = useState([]);
+
+  const url = "https://dummyapi.online/api/movies";
+
+  useEffect(()=>{
+    axios.get(`${url}`).then(
+      function(res){
+      //  console.log(res.data)
+       setMovies(res.data)
+      }
+    )
+  },[])
+
   return (
     <div>
     <div className='flex justify-center m-5 text-2xl font-bold'>
       Trending Movies
     </div>
     <div className='flex  flex-row flex-wrap  justify-around gap-3'>
-          <MoviesCard/>
-          <MoviesCard/>
-          <MoviesCard/>
-          <MoviesCard/>
-
-          <MoviesCard/>
-          <MoviesCard/>
-          <MoviesCard/>
-          <MoviesCard/>
+      {
+        Movies.map((movies) =>{
+          // console.log(movies.image,"imkdjf")
+          return <MoviesCard key={movies.id} path={`${url}/${movies.image}`} name={movies.movie}/>
+        })
+      }
+          
     </div>
     </div>
   )
--- src/Component/MoviesCard.jsx
+++ src/Component/MoviesCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
-const MoviesCard = () => {
+const MoviesCard = ({path, name}) => {
+
+   console.log(path,"path")
+ 
+  //  const imageUrl = `https://dummyapi.online/api/${path}`;
+  const imageUrl = path;
+  
+
   return (
     <div 
     className='h-[40vh] w-[200px] bg-center bg-cover flex-row  rounded-xl hover:cursor-pointer hover:scale-110 duration-300'
-     style={{ backgroundImage:`url(https://t3.ftcdn.net/jpg/05/06/01/42/360_F_506014246_V1vbPMYo0rOu06xtNOb7pIC1PEy275bC.jpg)`}}
+    //  style={{ backgroundImage:`url("https://dummyapi.online/api/${path}")`}}
+     style={{ backgroundImage: `url("${imageUrl}")`, 
+     border: '1px solid #ccc',
+     }}
     >
-
-
+      <div>{name}</div>
 
     </div>
   )
